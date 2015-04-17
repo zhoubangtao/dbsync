1dd3355fee2473fa57b026d8c8aac2d6b89edd92
Nathan <bangtao.zhou@17zuoye.com>
2015-04-17 19:13:05 +0800

    test logger

--- dbsync/__init__.py
+++ dbsync/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding:utf-8 -*-
 __author__ = 'nathan'
+import logging
 
 release = "0.1.1"
+
+logging.getLogger("dbsync")
--- dbsync/syncs/pool.py
+++ dbsync/syncs/pool.py
@@ -4,6 +4,7 @@ __author__ = 'nathan'
 from dbsync.syncs.base import BaseSync
 
 class ThreadPoolSync(BaseSync):
+
     pass
 
 
