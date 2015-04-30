c715fd73a76a00778324d1d19b74379a0d4e1591
Bangtao.Zhou <zhoubangtao@163.com>
2015-04-30 18:43:10 +0800

    add mail and mq notifier

--- dbsync/notifiers/mail.py
+++ dbsync/notifiers/mail.py
@@ -1,2 +1,11 @@
 # -*- coding:utf-8 -*-
 __author__ = 'nathan'
+
+from base import BaseNotifier
+
+
+class MailNotifier(BaseNotifier):
+    """use mail to send a notification"""
+
+    def _notify(self):
+        pass
