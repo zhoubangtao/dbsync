a1bbae10c78c165c1013ef65a18eee1b87c3c788
Bangtao.Zhou <zhoubangtao@163.com>
2015-05-13 19:02:59 +0800

    add DBSync and Engine

--- dbsync/__init__.py
+++ dbsync/__init__.py
@@ -5,3 +5,33 @@ import logging
 release = "0.1.1"
 
 logging.getLogger("dbsync")
+
+
+class DBSync():
+
+    def __init__(self):
+        pass
+
+    def syncer(self, syncer):
+        return self
+
+    def validator(self, validator):
+        return self
+
+    def serializer(self, serializer):
+        return self
+
+    def notifier(self, notifier):
+        return self
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+
+class Engine():
+
+    def __init__(self):
+        pass
