dfdc7ac0465836e09dbafc8933689f8f17c5e783
Bangtao.Zhou <zhoubangtao@163.com>
2015-05-13 19:02:10 +0800

    test dump data in sqlachemy without defining schema

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,11 @@ __author__ = 'nathan'
 
 import unittest
 import pytest
+import sqlite3
+from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine
+import json
+import collections
 
 try:
     from unittest.mock import Mock, MagicMock
@@ -10,9 +15,34 @@ except ImportError:
 
 
 class MyTestCase(unittest.TestCase):
-    def test_(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        engine = create_engine('sqlite:///:memory:', echo=True)
+        metadata = MetaData()
+        users = Table('users', metadata,
+            Column('ID', Integer, primary_key=True),
+            Column('name', String),
+            Column('fullname', String),
+        )
+        metadata.create_all(engine)
 
+        ins = users.insert().values(name='jack', fullname='Jack Jones')
+        self.conn = engine.connect()
+        result = self.conn.execute(ins)
+
+    def test_sqlachemy(self):
+        # self.assertEqual(True, False)
+        users = self.conn.execute("select * from users")
+        print users.__module__
+        print dir(users)
+        for row in users:
+            print dir(row)
+            print row.keys()
+            print json.dumps(collections.OrderedDict((key.lower(), row[key]) for key in row.keys()))
+
+        # print json.dumps(dict(users.first().items()))
+
+    def tearDown(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
