e20c7c9051bae6fdc6595ae79f05c1b3460866ec
Bangtao.Zhou <zhoubangtao@163.com>
2015-05-20 16:49:17 +0800

    to support hive

--- dbsync/stores/hive.py
+++ dbsync/stores/hive.py
@@ -1,2 +1,18 @@
 # -*- coding:utf-8 -*-
 __author__ = 'nathan'
+
+from .base import BaseStore
+
+
+class HiveTableStore(BaseStore):
+    """
+        It stands for Hive table
+    """
+    pass
+
+
+class HivePartitionStore(BaseStore):
+    """
+        It stands for a Hive Partition
+    """
+    pass
