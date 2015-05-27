0a72b35cb9728cba1f5ee2a4c96bec0a35f6999d
Bangtao.Zhou <zhoubangtao@163.com>
2015-05-27 20:01:41 +0800

    the default table syncer

--- dbsync/syncers/pool.py
+++ dbsync/syncers/pool.py
@@ -6,16 +6,17 @@ from base import BaseSyncer, sync_data, incr_sync_by_day
 
 
 class BasePoolSyncer(BaseSyncer):
-    def __init__(self, store, pool, serializer=None, notifier=None):
-        super(BasePoolSyncer, self).__init__(store, serializer, notifier)
+    def __init__(self, pool, serializer=None, notifier=None):
+        super(BasePoolSyncer, self).__init__(None, serializer, notifier)
         self._pool = pool
 
-    def sync(self):
-        self._pool.submit(sync_data, self._store, self._serializer, 'data.txt')
+    def sync(self, store, local_path, hdfs_path):
+        print local_path, hdfs_path
+        self._pool.submit(sync_data, store, local_path, hdfs_path, self._serializer)
         # sync_data(self._store, self._serializer, 'data.txt')
 
-    def incr_sync_by_day(self):
-        self._pool.submit(incr_sync_by_day, self._store, self, self._serializer)
+    def incr_sync_by_day(self, store, local_path, hdfs_path):
+        self._pool.submit(incr_sync_by_day, store, local_path, hdfs_path, self._serializer)
 
     def stop(self):
         self._pool.shutdown()
@@ -30,9 +31,9 @@ class ThreadPoolSyncer(BasePoolSyncer):
     :param max_workers: the maximum number of spawned threads.
     """
 
-    def __init__(self, store, serializer=None, notifier=None, max_workers=10):
+    def __init__(self, serializer=None, notifier=None, max_workers=10):
         pool = concurrent.futures.ThreadPoolExecutor(int(max_workers))
-        super(ThreadPoolSyncer, self).__init__(store, pool, serializer, notifier)
+        super(ThreadPoolSyncer, self).__init__(pool, serializer, notifier)
 
 
 class ProcessPoolSyncer(BasePoolSyncer):
@@ -44,7 +45,7 @@ class ProcessPoolSyncer(BasePoolSyncer):
     :param max_workers: the maximum number of spawned processes.
     """
 
-    def __init__(self, store, serializer=None, notifier=None, max_workers=10):
+    def __init__(self, serializer=None, notifier=None, max_workers=10):
         pool = concurrent.futures.ProcessPoolExecutor(int(max_workers))
-        super(ProcessPoolSyncer, self).__init__(store, pool, serializer, notifier)
+        super(ProcessPoolSyncer, self).__init__(pool, serializer, notifier)
 
