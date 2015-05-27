f55f11d4e1b00b597c924ae4358d657bdbc899b5
Bangtao.Zhou <zhoubangtao@163.com>
2015-05-27 19:56:49 +0800

    MongoStore stands for a mongo collection, which can get all data by method all, or get increment data

--- dbsync/stores/mongo.py
+++ dbsync/stores/mongo.py
@@ -12,14 +12,19 @@ class MongoStore(BaseStore):
         It stands for a table or a sql result, split to some javascript snippets
     """
 
-    def __init__(self, client, database, collection):
-        self._coll = client[database][collection]
+    def __init__(self, datasource, database, collection, create_column=None, update_column=None):
+        super(MongoStore, self).__init__(datasource, database, collection,
+                                         create_column=create_column,
+                                         update_column=update_column)
+
+        client = pymongo.MongoClient(host=datasource.host, port=datasource.port)
+        self._collection = client[database][collection]
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        for record in self._coll.find():
+        for record in self._collection.find():
             yield record
 
     next = __next__
@@ -28,7 +33,7 @@ class MongoStore(BaseStore):
         # client = pymongo.MongoClient(hostname, port, tz_aware=True)
 
         # res = client[database][collection].find()
-        res = self._coll.find()
+        res = self._collection.find()
 
         for record in res:
             yield record
@@ -38,7 +43,7 @@ class MongoStore(BaseStore):
 
     def incr_by_day(self):
         starttime, endtime = get_day_range()
-        res = self._coll.find({'$or': {'create_time': {'$gte': starttime, '$lte': endtime},
+        res = self._collection.find({'$or': {'create_time': {'$gte': starttime, '$lte': endtime},
                                        'update_time': {'$gte': starttime, '$lte': endtime}}})
 
         for record in res:
